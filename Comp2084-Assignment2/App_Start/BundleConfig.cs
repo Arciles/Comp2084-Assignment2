7d913f372f76f8e1097d272ef7b08e6144ebf7db
Esat IBIS <esat.taha.ibis@outlook.com>
2016-11-28 17:56:46 -0500

    Custom style sheet added to the project. Layouts updated based on the new style rules. Copule of new pics added

--- Comp2084-Assignment2/App_Start/BundleConfig.cs
+++ Comp2084-Assignment2/App_Start/BundleConfig.cs
@@ -13,7 +13,7 @@ namespace Comp2084_Assignment2
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
-
+            
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -23,9 +23,14 @@ namespace Comp2084_Assignment2
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            // Add custom styles to the bundle
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/main.css",
                       "~/Content/site.css"));
+
+            
+
         }
     }
 }
