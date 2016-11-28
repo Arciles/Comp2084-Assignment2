423ad64e4b50e6c109ef0e7a24be67356bfc3fed
IBIS, Esat <esatibis@expertech.net>
2016-11-28 08:09:08 -0500

    Add project files.

---
+++ Comp2084-Assignment2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Comp2084_Assignment2.Startup))]
+namespace Comp2084_Assignment2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
